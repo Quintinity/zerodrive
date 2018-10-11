f3d9763b677ad0d058d3f048fd9a500a0fd0a14b
Quintinity <vlad.marica1@gmail.com>
2018-10-11 09:29:56 -0300

    Updated test script and added env backed settings

--- src/app/config.py
+++ src/app/config.py
@@ -8,14 +8,32 @@
 # Default configuration. Change this for every machine if required, but don't commit changes..
 # git update-index --assume-unchanged src/app/config.py
 
-db = {
-    "name": "zerodrive",
-    "host": "localhost",
-    "port": 4445,
-    "user": "user",
-    "password": "password"
-}
+import os
 
-server = {
-    "port": 40500
-}
+# Environment variable backed dictionary
+# When looking up a key, it checks if a matching environment variable
+# named "ZERODRIVE_<dict name>_<key>" exists. If so, it returns the environment
+# variable's value, otherwise it uses the default value in the dict.
+class envdict(dict):
+    def __init__(self, name):
+        dict.__init__(self)
+        self.name = name
+
+    def __getitem__(self, key):
+        env_var_name = "zerodrive_{}_{}".format(self.name, key).upper()
+        default_val = dict.__getitem__(self, key)
+        val = os.environ.get(env_var_name, default_val)
+        if default_val is int:
+            val = int(val)
+        return val
+
+# Default configuration
+db = envdict("db")
+db["name"] = "zerodrive"
+db["host"] = "localhost"
+db["port"] = 4445
+db["user"] = "user"
+db["password"] = "password"
+
+server = envdict("server")
+server["port"] = 40500
--- src/test.sh
+++ src/test.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
-HOST=http://127.0.0.1:40500
+IP=127.0.0.1
+PORT=40500
+BASEURL=http://$IP:$PORT
 
-set -x
+# Use netcat to scan port to check if the server is running
+nc -z $IP $PORT
+if [ "$?" = "1" ]; then
+    echo "Server not running at $BASEURL"
+    exit 1
+fi
+
+TOTAL_TEST_CASES=0
+FAILED_TEST_CASES=0
 
 function testcase() {
+    title="$1"
     shift;
+    expected_code="$1"
     shift;
-    result=$("$@")
-    echo result
+    code=$("$@" -w "%{http_code}" -o /dev/null)
+
+    RESULT=" OK "
+    COLOR=$(tput setaf 2)
+    COLOR_RESET=$(tput sgr0)
+    MSG=
+    
+    if [ "$code" != "$expected_code" ]; then
+        RESULT="FAIL"
+        COLOR=$(tput setaf 1)
+        FAILED_TEST_CASES=$(($FAILED_TEST_CASES + 1))
+    fi
+    TOTAL_TEST_CASES=$(($TOTAL_TEST_CASES + 1))
+
+    echo "[${COLOR}${RESULT}${COLOR_RESET}] ${title}"
 }
 
-testcase "Test user creation" 200 curl -s -H "Content-Type: application/json" -X POST -d '{"vmarica": "a"}' $HOST/user
+testcase "Test user creation" 200 curl -s -H "Content-Type: application/json" -X POST -d '{"vmarica": "a"}' $BASEURL/user
+
+if [[ $FAILED_TEST_CASES -gt 0 ]]; then
+    exit 1
+fi
