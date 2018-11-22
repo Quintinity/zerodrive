772594fc48eb632a0ca3cbec920ee6c272c988ad
Quintinity <vlad.marica1@gmail.com>
2018-11-22 10:25:50 -0400

    Setup Vue Router and 404 page

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,16 @@
-new Vue({
-    el: "#app"
+const homepage = {
+   
+};
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        { path: "/", component: { template: "#homepage-template" } },
+        { path: "*", component: { template: "#page-404-template" } }
+    ]
+});
+
+const app = new Vue({
+    el: "#app",
+    router: router
 });
